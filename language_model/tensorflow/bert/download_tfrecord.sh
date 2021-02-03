30b5a96e3331592eaf92a33e7b1b9465d43847e2
Taeksoo Kim <ts.kim@kaist.ac.kr>
2021-02-03 22:27:20 +0900

    Add script for downloading tfrecord

---
+++ language_model/tensorflow/bert/download_tfrecord.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p tfrecord
+cd tfrecord
+wget -O "part-00230-of-00500" "https://kaistackr-my.sharepoint.com/:u:/g/personal/ts_kim_kaist_ac_kr/EdM4tonKmBdHvcu6WZDt2psBJAWXnlPby2De60R_E7zwEA?download=1"
