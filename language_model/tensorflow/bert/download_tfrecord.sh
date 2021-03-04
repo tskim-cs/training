a709e2730087df147ecb82ad4a07f93e1090925c
Taeksoo Kim <ts.kim@kaist.ac.kr>
2021-03-05 01:51:25 +0900

    Change download path to Google Drive

--- language_model/tensorflow/bert/download_tfrecord.sh
+++ language_model/tensorflow/bert/download_tfrecord.sh
@@ -2,4 +2,5 @@
 
 mkdir -p tfrecord
 cd tfrecord
-wget -O "part-00230-of-00500" "https://kaistackr-my.sharepoint.com/:u:/g/personal/ts_kim_kaist_ac_kr/EdM4tonKmBdHvcu6WZDt2psBJAWXnlPby2De60R_E7zwEA?download=1"
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1k24WvGQRV8GUe1-hR_2VksrAgLTqdVLQ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1k24WvGQRV8GUe1-hR_2VksrAgLTqdVLQ" -O "part-00230-of-00500" && rm -rf /tmp/cookies.txt
